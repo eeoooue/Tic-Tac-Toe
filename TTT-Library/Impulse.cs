44f3d9ecdfd5c5b19621864e5bd32bfa68820f3e
eeoooue <pdmoorhouse@gmail.com>
2023-05-15 13:11:33 +0100

    created Impulse class for exploring the board along a direction

---
+++ TTT-Library/Impulse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTT_Library
+{
+    public class Impulse
+    {
+        private int _deltaI;
+        private int _deltaJ;
+        public Impulse(int di, int dj)
+        {
+            _deltaI = di;
+            _deltaJ = dj;
+        }
+
+        public void Translate(ref int i, ref int j)
+        {
+            i += _deltaI;
+            j += _deltaJ;
+        }
+    }
+}

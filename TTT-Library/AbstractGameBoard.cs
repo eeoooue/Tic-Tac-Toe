299a547c073b21350055f65b1da96d31999b301b
eeoooue <pdmoorhouse@gmail.com>
2023-05-16 18:18:34 +0100

    made a GameBoard abstraction to inherit from

---
+++ TTT-Library/AbstractGameBoard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTT_Library
+{
+    public abstract class AbstractGameBoard
+    {
+        protected AbstractTile[,] _tiles = new AbstractTile[3, 3];
+
+        public AbstractGameBoard() { }
+
+        public AbstractTile GetTile(int row, int column)
+        {
+            return _tiles[row, column];
+        }
+    }
+}
--- TTT-Library/GameBoard.cs
+++ TTT-Library/GameBoard.cs
@@ -6,26 +6,17 @@ using System.Threading.Tasks;
 
 namespace TTT_Library
 {
-    public class GameBoard
+    public class GameBoard : AbstractGameBoard
     {
-        public BoardTile[,] Tiles { get; private set; } 
-
         public GameBoard(TicTacToeGame myGame) 
         {
-            Tiles = new BoardTile[3, 3];
-
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    Tiles[i, j] = new BoardTile(myGame, i, j);
+                    _tiles[i, j] = new BoardTile(myGame, i, j);
                 }
             }
-        }
-
-        public BoardTile GetTile(int i, int j)
-        {
-            return Tiles[i, j];
         }
     }
 }

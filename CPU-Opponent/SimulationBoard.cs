b9e2c24f42623b1e65da7dc34549548004267cf9
eeoooue <pdmoorhouse@gmail.com>
2023-05-16 18:21:19 +0100

    outlined a SimulationBoard class to use with Judge

---
+++ CPU-Opponent/SimulationBoard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TTT_Library;
+
+namespace CPU_Opponent
+{
+    internal class SimulationBoard : AbstractGameBoard
+    {
+
+        public SimulationBoard(GameSimulation sim)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    _tiles[i, j] = new SimulationTile(sim, i, j);
+                }
+            }
+        }
+
+    }
+}

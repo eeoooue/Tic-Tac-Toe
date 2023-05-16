4bb7384d55c8e43b41bd433573c031c72441c3bc
eeoooue <pdmoorhouse@gmail.com>
2023-05-16 19:49:54 +0100

    added null check to GetMoveCount()
    need to revisit why this was needed

--- CPU-Opponent/SimulationBoard.cs
+++ CPU-Opponent/SimulationBoard.cs
@@ -9,17 +9,36 @@ namespace CPU_Opponent
 {
     internal class SimulationBoard : AbstractGameBoard
     {
-
-        public SimulationBoard(GameSimulation sim)
+        public void CopyBoard(GameSimulation sim, AbstractGameBoard board)
         {
-            for (int i = 0; i < 3; i++)
+            for(int i=0; i < _rows; i++)
             {
-                for (int j = 0; j < 3; j++)
+                for(int j=0; j < _cols; j++)
                 {
-                    _tiles[i, j] = new SimulationTile(sim, i, j);
+                    AbstractTile originalTile = board.GetTile(i, j);
+                    SimulationTile simTile = new(sim, i, j);
+                    simTile.SetTeam(originalTile.Character);
+
+                    _tiles[i, j] = simTile;
                 }
             }
         }
 
+        public int GetMoveCount()
+        {
+            int count = 0;
+            foreach(AbstractTile tile in _tiles)
+            {
+                if (tile != null)
+                {
+                    if (tile.Character != ' ')
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
     }
 }

83eece33e2b8b141490f5a482014165f306784a4
eeoooue <pdmoorhouse@gmail.com>
2023-05-16 20:42:46 +0100

    renamed BoardTile to ClickableTile, Tiles are now Mark() able

---
+++ TTT-Library/ClickableTile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTT_Library
+{
+    public class ClickableTile : AbstractTile
+    {
+        
+        private TicTacToeGame _game;
+
+        public ClickableTile(TicTacToeGame myGame, int i, int j) : base(i, j)
+        {
+            _game = myGame;
+        }
+
+        public override void Mark()
+        {
+            if (!Marked && !_game.GameOver)
+            {
+                Character = _game.CurrentPlayer;
+                Marked = true;
+            }
+        }
+
+        public void Click()
+        {
+            if (!Marked && !_game.GameOver)
+            {
+                PlayerMove move = new PlayerMove(Row, Column, _game.CurrentPlayer);
+                _game.SubmitMove(move);
+            }
+        }
+    }
+}

75458b3410344cbd155e9395f545eaebe18940b0
eeoooue <pdmoorhouse@gmail.com>
2023-05-16 16:58:21 +0100

    created SimulationGame & SimulationTile child classes

---
+++ CPU-Opponent/SimulatedGame.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TTT_Library;
+
+namespace CPU_Opponent
+{
+    internal class SimulatedGame : TicTacToeGame
+    {
+        private readonly Stack<PlayerMove> _moves = new Stack<PlayerMove>();
+
+        public void CloneBoard(GameBoard gameBoard)
+        {
+            foreach(BoardTile tile in gameBoard.Tiles)
+            {
+                int i = tile.Row;
+                int j = tile.Column;
+
+                SimulationTile simTile = new SimulationTile(this, i, j);
+                simTile.SetTeam(tile.Character);
+                Board.Tiles[i, j] = simTile;
+            }
+        }
+
+        private bool CanMove(int i, int j)
+        {
+            BoardTile tile = Board.GetTile(i, j);
+            return !tile.Clicked;
+        }
+
+        public bool SimulateMove(PlayerMove move)
+        {
+            if (CanMove(move.Row, move.Column))
+            {
+                SubmitMove(move);
+                _moves.Push(move);
+                return true;
+            }
+            return false;
+        }
+
+        public void UndoPreviousMove()
+        {
+            if (_moves.Count > 0)
+            {
+                PlayerMove move = _moves.Pop();
+                ResetTile(move.Row, move.Column);
+            }
+        }
+
+        private void ResetTile(int row, int column)
+        {
+            Board.Tiles[row, column] = new SimulationTile(this, row, column);
+        }
+    }
+}
---
+++ CPU-Opponent/SimulationTile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TTT_Library;
+
+namespace CPU_Opponent
+{
+    internal class SimulationTile : BoardTile
+    {
+        public SimulationTile(SimulatedGame game, int i, int j) : base(game, i, j) { }
+
+        public void SetTeam(char team)
+        {
+            Character = team;
+        }
+    }
+}

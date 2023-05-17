d5aef1d9dde7b27f6882099fba6eab0faaf4026f
eeoooue <pdmoorhouse@gmail.com>
2023-05-17 20:27:39 +0100

    started a WinForms project with clickable buttons

---
+++ Tic-Tac-Toe/Win-Forms-App/ButtonTile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win_Forms_App
+{
+    public class ButtonTile : Button
+    {
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+
+        public ButtonTile(int i, int j)
+        {
+            Row = i;
+            Column = j;
+            Font = new Font("Microsoft Sans Serif", 36F);
+            Size = new Size(100, 100);
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+            Text = "X";
+        }
+    }
+}
---
+++ Tic-Tac-Toe/Win-Forms-App/TicTacToe.cs
@@ -0,0 +1,33 @@
+namespace Win_Forms_App
+{
+    public partial class TicTacToe : Form
+    {
+        public List<ButtonTile> Buttons { get; set; }
+
+        public TicTacToe()
+        {
+            InitializeComponent();
+            Buttons = new List<ButtonTile>();
+            BuildButtons();
+        }
+
+        private void BuildButtons()
+        {
+            Buttons = new List<ButtonTile>();
+
+            for(int i=0; i<3; i++)
+            {
+                for(int j=0; j<3; j++)
+                {
+                    ButtonTile btn = new ButtonTile(i, j);
+                   
+                    int x_position = 240 + (115 * j);
+                    int y_position = 65 + (115 * i);
+                    btn.Location = new Point(x_position, y_position);
+                    Buttons.Add(btn);
+                    Controls.Add(btn);
+                }
+            }
+        }
+    }
+}

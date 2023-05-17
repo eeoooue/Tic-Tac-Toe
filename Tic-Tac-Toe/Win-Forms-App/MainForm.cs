4ced960456061beebff48b229ddaba3adbc69a4d
eeoooue <pdmoorhouse@gmail.com>
2023-05-17 20:50:19 +0100

    MainForm has a GameAgainstCPU

--- Tic-Tac-Toe/Win-Forms-App/MainForm.cs
+++ Tic-Tac-Toe/Win-Forms-App/MainForm.cs
@@ -1,12 +1,17 @@
+using CPU_Opponent;
+using Game_Library;
+
 namespace Win_Forms_App
 {
     public partial class MainForm : Form
     {
+        public TicTacToeGame _myGame;
         public List<ButtonTile> Buttons { get; set; }
 
         public MainForm()
         {
             InitializeComponent();
+            _myGame = new GameAgainstCPU();
             Buttons = new List<ButtonTile>();
             BuildButtons();
         }
@@ -19,14 +24,23 @@ namespace Win_Forms_App
             {
                 for(int j=0; j<3; j++)
                 {
-                    ButtonTile btn = new ButtonTile(i, j);
-                   
+                    GameTile boardTile = _myGame.Board.GetTile(i, j);
+                    ButtonTile btn = new(_myGame, this, boardTile);
+                    
                     int x_position = 240 + (115 * j);
                     int y_position = 65 + (115 * i);
                     btn.Location = new Point(x_position, y_position);
                     Buttons.Add(btn);
                     Controls.Add(btn);
                 }
+            }
+        }
+
+        public void UpdateAll()
+        {
+            foreach(ButtonTile btn in Buttons)
+            {
+                btn.UpdateMe();
             }
         }
     }

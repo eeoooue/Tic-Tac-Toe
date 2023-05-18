05ad856ff5b6f2fc032bb50e11622ae3bfd7f0e2
eeoooue <pdmoorhouse@gmail.com>
2023-05-18 16:42:07 +0100

    MainPage now has a grid of clickable tiles

---
+++ Tic-Tac-Toe/MAUI-App/ButtonTile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI_App
+{
+    internal class ButtonTile : Button
+    {
+
+        public ButtonTile()
+        {
+            Text = "X";
+            FontSize = 50;
+            Clicked += ClickAction;
+        }
+
+        private void ClickAction(object sender, EventArgs e)
+        {
+            Text = "O";
+        }
+
+    }
+}
--- Tic-Tac-Toe/MAUI-App/MainPage.xaml.cs
+++ Tic-Tac-Toe/MAUI-App/MainPage.xaml.cs
@@ -7,18 +7,27 @@
         public MainPage()
         {
             InitializeComponent();
+            CreateButtons();
+        }
+
+        private void CreateButtons()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    ButtonTile tile = new();
+                    GameBoard.Children.Add(tile);
+                    Grid.SetRow(tile, i);
+                    Grid.SetColumn(tile, j);
+                }
+            }
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
             count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 }

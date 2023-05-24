05d25214764b3435d752988f3355b8dac940cb5d
eeoooue <pdmoorhouse@gmail.com>
2023-05-24 16:29:24 +0100

    judge can now check for winning game states

--- Tic-Tac-Toe/CPP-TestProject/Judge.cpp
+++ Tic-Tac-Toe/CPP-TestProject/Judge.cpp
@@ -7,8 +7,10 @@ using namespace std;
 int Judge::CountMoves(vector<vector <char>> board) {
 
     int count = 0;
+
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
+
             if (board[i][j] != ' ') {
                 count++;
             }
@@ -21,7 +23,76 @@ int Judge::CountMoves(vector<vector <char>> board) {
 
 bool Judge::FindsWinner(vector<vector <char>> board) {
 
+    for (int i = 0; i < 3; i++) {
+
+        if (IsWinningRow(board, i)) {
+            return true;
+        }
+
+        if (IsWinningColumn(board, i)) {
+            return true;
+        }
+    }
+
+    return HasWinningDiagonal(board);
+}
 
 
-    return true;
+bool Judge::IsWinningRow(vector<vector <char>> board, int i) {
+
+    char team = board[i][0];
+
+    if (team == ' ') {
+        return false;
+    }
+
+    if (board[i][1] == team) {
+        if (board[i][2] == team) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+bool Judge::IsWinningColumn(vector<vector <char>> board, int j) {
+
+    char team = board[0][j];
+
+    if (team == ' ') {
+        return false;
+    }
+
+    if (board[1][j] == team) {
+        if (board[2][j] == team) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+bool Judge::HasWinningDiagonal(vector<vector <char>> board) {
+
+    char team = board[1][1];
+
+    if (team == ' ') {
+        return false;
+    }
+
+    if (board[0][0] == team) {
+        if (board[2][2] == team) {
+            return true;
+        }
+    }
+
+    if (board[0][2] == team) {
+        if (board[2][0] == team) {
+            return true;
+        }
+    }
+
+    return false;
 }
--- Tic-Tac-Toe/CPP-TestProject/Judge.h
+++ Tic-Tac-Toe/CPP-TestProject/Judge.h
@@ -10,5 +10,8 @@ public:
 
 	int CountMoves(vector<vector <char>> board);
 	bool FindsWinner(vector<vector <char>> board);
+	bool IsWinningRow(vector<vector <char>> board, int i);
+	bool IsWinningColumn(vector<vector <char>> board, int j);
+	bool HasWinningDiagonal(vector<vector <char>> board);
 };
 

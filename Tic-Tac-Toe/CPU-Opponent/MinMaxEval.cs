3bd133cab34d741503399ee5150a574f9db7bbf4
eeoooue <pdmoorhouse@gmail.com>
2023-05-24 03:24:08 +0100

    CPU-Opponent's EvaluatedMove class is now readonly MinMaxEval struct

---
+++ Tic-Tac-Toe/CPU-Opponent/MinMaxEval.cs
@@ -0,0 +1,25 @@
+﻿using Game_Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPU_Opponent
+{
+    internal readonly struct MinMaxEval
+    {
+        public readonly int i;
+        public readonly int j;
+        public readonly int score;
+
+        public MinMaxEval(int i, int j, int score)
+        {
+            this.i = i;
+            this.j = j;
+            this.score = score;
+        }
+
+        public MinMaxEval(PlayerMove move, int score) : this(move.Row, move.Column, score) { }
+    }
+}

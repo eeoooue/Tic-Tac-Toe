c4169f15e7c349b47415d7da662f80f2cfcaf28c
eeoooue <pdmoorhouse@gmail.com>
2023-05-24 18:56:09 +0100

    created a console app for testing the cpp wrapper

---
+++ Tic-Tac-Toe/Test-Console/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Test_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This project will test my CPP-Wrapper");
+
+
+
+        }
+    }
+}

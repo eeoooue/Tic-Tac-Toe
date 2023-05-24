b6ba917bc9a1e9fefdd790121b813bd6a62bc82a
eeoooue <pdmoorhouse@gmail.com>
2023-05-24 20:23:59 +0100

    changes to solution files

--- Tic-Tac-Toe/CPP-Opponent/CPP-Opponent.cpp
+++ Tic-Tac-Toe/CPP-Opponent/CPP-Opponent.cpp
@@ -1,13 +1,24 @@
 // CPP-Opponent.cpp : Defines the exported functions for the DLL.
-//
 
 #include "pch.h"
 #include "framework.h"
 #include "CPP-Opponent.h"
+#include <string>
+#include "Solver.h"
 
+using namespace std;
 
 // This is an example of an exported function.
 extern "C" CPPOPPONENT_API int fnCPPOpponent(void)
 {
     return 56;
+}
+
+// This is an example of an exported function.
+extern "C" CPPOPPONENT_API string GetMoveString(string boardState)
+{
+    Solver solver;
+    string bestMove = solver.GetBestMove(boardState);
+
+    return bestMove;
 }

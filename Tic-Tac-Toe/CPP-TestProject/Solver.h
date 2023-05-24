b5db4efbdde0b61b1193aa944be346064686c9ee
eeoooue <pdmoorhouse@gmail.com>
2023-05-24 16:08:16 +0100

    changes to Solver header methods

--- Tic-Tac-Toe/CPP-TestProject/Solver.h
+++ Tic-Tac-Toe/CPP-TestProject/Solver.h
@@ -1,8 +1,22 @@
 #pragma once
+
+#include "MinMaxEval.h"
+
+#include <vector>
+
+using namespace std;
+
+
 class Solver
 {
 	public:
-		void GetBestMove();
-		void MinMaxExplore();
+		void GetBestMove(vector<vector <char>> board);
+		MinMaxEval MinMaxExplore(vector<vector <char>> board, char team, int moves);
+
+		MinMaxEval GetDummyEval(int score);
+		MinMaxEval BuildEval(int i, int j, int score);
+
+		char GetTurnPlayer(int moves);
+
 };
 

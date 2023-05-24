b5008f016af79b5d320c8c8bcf17d2c3f746c71b
eeoooue <pdmoorhouse@gmail.com>
2023-05-24 20:15:55 +0100

    created a CPPOpponent child of Opponent with a WrappedSolver

---
+++ Tic-Tac-Toe/CPP-Wrapper/CPPOpponent.cs
@@ -0,0 +1,41 @@
+﻿using Game_Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPP_Wrapper
+{
+    public class CPPOpponent : Opponent
+    {
+        private WrappedSolver Solver { get; set; }
+
+        public CPPOpponent(char team) : base(team)
+        {
+            Solver = new WrappedSolver(team);
+        }
+
+        public override PlayerMove MakeMove(TicTacToeGame game)
+        {
+            string boardState = GetBoardstateString(game.Board);
+            return Solver.GetBestMove(boardState);
+        }
+
+        private string GetBoardstateString(GameBoard board)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    GameTile tile = board.GetTile(i, j);
+                    sb.Append(tile.Character);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Tic-Tac-Toe/CPP-Wrapper/WrappedSolver.cs
@@ -0,0 +1,38 @@
+﻿using Game_Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPP_Wrapper
+{
+    internal class WrappedSolver
+    {
+        [DllImport("CPPOpponent.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int fnCPPOpponent();
+
+        private char Team { get; set; }
+
+        public WrappedSolver(char team)
+        {
+            Team = team;
+        }
+
+        public PlayerMove GetBestMove(string boardState)
+        {
+            string move = "0 0";
+            return ParseMove(move);
+        }
+
+        private PlayerMove ParseMove(string moveString)
+        {
+            string[] arr = moveString.Split(' ');
+            int i = int.Parse(arr[0]);
+            int j = int.Parse(arr[1]);
+
+            return new PlayerMove(i, j, Team);
+        }
+    }
+}

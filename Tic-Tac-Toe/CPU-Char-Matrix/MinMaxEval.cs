8e4de38a9e52be9f7027da050ac0ea17b80be3ce
eeoooue <pdmoorhouse@gmail.com>
2023-05-24 01:27:28 +0100

    created a MinMaxEval class for the char matrix solver

---
+++ Tic-Tac-Toe/CPU-Char-Matrix/MinMaxEval.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPU_Char_Matrix
+{
+    internal class MinMaxEval
+    {
+        // in cpp we'll just do this with a string or a struct
+        public int I { get; private set; }
+        public int J { get; private set; }
+        public int Score { get; private set; }
+
+        public MinMaxEval(int i, int j, int score)
+        {
+            I = i;
+            J = j;
+            Score = score;
+        }
+    }
+}

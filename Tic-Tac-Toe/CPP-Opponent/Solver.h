3d16e1287e42000bab92d59a0651e0e8bb824fa0
eeoooue <pdmoorhouse@gmail.com>
2023-05-24 20:23:36 +0100

    copied Solver class into DLL project

---
+++ Tic-Tac-Toe/CPP-Opponent/Solver.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "MinMaxEval.h"
+#include "Judge.h"
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+class Solver
+{
+private:
+	Judge judge;
+	char GetTurnPlayer(const int moves);
+	MinMaxEval MinMaxExplore(vector<vector <char>> board, const char team, const int moves);
+	vector<vector <char>> UnpackBoard(const string line);
+	string UnpackEvaluation(const MinMaxEval evaluation);
+
+public:
+	string GetBestMove(const string line);
+};
+

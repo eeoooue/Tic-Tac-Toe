679583835a8d157eb56b244ebaacc4d5790b9f04
eeoooue <pdmoorhouse@gmail.com>
2023-05-24 18:32:14 +0100

    just indenting

--- Tic-Tac-Toe/CPP-TestProject/Judge.h
+++ Tic-Tac-Toe/CPP-TestProject/Judge.h
@@ -6,13 +6,13 @@ using namespace std;
 class Judge
 {
 
-private:
-	bool IsWinningRow(const vector<vector <char>> board, const int i);
-	bool IsWinningColumn(const vector<vector <char>> board, const int j);
-	bool HasWinningDiagonal(const vector<vector <char>> board);
+	private:
+		bool IsWinningRow(const vector<vector <char>> board, const int i);
+		bool IsWinningColumn(const vector<vector <char>> board, const int j);
+		bool HasWinningDiagonal(const vector<vector <char>> board);
 
-public:
-	int CountMoves(const vector<vector <char>> board);
-	bool FindsWinner(const vector<vector <char>> board);
+	public:
+		int CountMoves(const vector<vector <char>> board);
+		bool FindsWinner(const vector<vector <char>> board);
 };
 

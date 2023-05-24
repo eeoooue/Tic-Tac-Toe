44c3cdce644ba3834ffa7117131e0103c2cc21eb
eeoooue <pdmoorhouse@gmail.com>
2023-05-25 00:16:15 +0100

    refactored ParseMove() method

--- Tic-Tac-Toe/CPP-Wrapper/WrappedSolver.cs
+++ Tic-Tac-Toe/CPP-Wrapper/WrappedSolver.cs
@@ -22,46 +22,14 @@ namespace CPP_Wrapper
 
         public PlayerMove GetBestMove(int boardStateInt)
         {
-            // string move = "0 0";
-
             int val = GetMoveString(boardStateInt);
-            string move = ParseDigits(val);
-            return ParseMove(move);
+            return ParseMove(val);
         }
 
-        private string ParseDigits(int input)
+        private PlayerMove ParseMove(int input)
         {
-            switch (input)
-            {
-                case 0:
-                    return "0 0";
-                case 1:
-                    return "0 1";
-                case 2:
-                    return "0 2";
-                case 10:
-                    return "1 0";
-                case 11:
-                    return "1 1";
-                case 12:
-                    return "1 2";
-                case 20:
-                    return "2 0";
-                case 21:
-                    return "2 1";
-                case 22:
-                    return "2 2";
-                default:
-                    return "0 0";
-            }
-        }
-
-        private PlayerMove ParseMove(string moveString)
-        {
-            string[] arr = moveString.Split(' ');
-            int i = int.Parse(arr[0]);
-            int j = int.Parse(arr[1]);
-
+            int i = input / 10;
+            int j = input % 10;
             return new PlayerMove(i, j, Team);
         }
     }
